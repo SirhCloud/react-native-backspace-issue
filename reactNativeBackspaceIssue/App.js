098714079c934861bd12690fdcba72bcc0fe4f3f
SirhCloud <18542577+SirhCloud@users.noreply.github.com>
2020-08-19 09:51:30 +1000

    Updated example with test cases outlined in the issue

--- reactNativeBackspaceIssue/App.js
+++ reactNativeBackspaceIssue/App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Text } from 'react-native';
 
 export default function App() {
   return (
@@ -22,6 +22,58 @@ export default function App() {
         }}
         onKeyPress={() => {
           console.log("3: onKeyPress");
+        }}
+      />
+      <Text>Letter</Text>
+      <TextInput
+        value="a"
+        onChange={() => {
+          console.log("Letter: onChange");
+        }}
+        onChangeText={() => {
+          console.log("Letter: onChangeText");
+        }}
+        onKeyPress={() => {
+          console.log("Letter: onKeyPress");
+        }}
+      />
+      <Text>Number</Text>
+      <TextInput
+        value="1"
+        onChange={() => {
+          console.log("Number: onChange");
+        }}
+        onChangeText={() => {
+          console.log("Number: onChangeText");
+        }}
+        onKeyPress={() => {
+          console.log("Number: onKeyPress");
+        }}
+      />
+      <Text>Space</Text>
+      <TextInput
+        value=" "
+        onChange={() => {
+          console.log("Space: onChange");
+        }}
+        onChangeText={() => {
+          console.log("Space: onChangeText");
+        }}
+        onKeyPress={() => {
+          console.log("Space: onKeyPress");
+        }}
+      />
+      <Text>Symbol</Text>
+      <TextInput
+        value="!"
+        onChange={() => {
+          console.log("Symbol: onChange");
+        }}
+        onChangeText={() => {
+          console.log("Symbol: onChangeText");
+        }}
+        onKeyPress={() => {
+          console.log("Symbol: onKeyPress");
         }}
       />
     </View>
